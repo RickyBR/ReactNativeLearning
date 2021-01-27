dac6cb6a01411447e4e771b6682ae2970bf82f34
RickyBR <berlandoricky@gmail.com>
2021-01-28 01:19:38 +0700

    Menambahkan Tugas

--- sanberApp/App.js
+++ sanberApp/App.js
@@ -1,21 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+  Platform,
+  Image,
+  TouchableOpacity,
+  FlatList
+} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <View style={styles.container}>
+      <View style={styles.navBar}>
+          <Image source={require('./assets/logo.png')} style={{width:100,height:22}}/>
+          <View style={styles.rightNav}>
+
+          </View>
+      </View>
+   </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
+  navBar : {
+    height : 55,
+    backgroundColor : 'white',
+    elevation: 3,
+    paddingHorizontal : 15,
+    flexDirection: 'row',
+    alignItems:'center'
+  }
 });
---
+++ sanberApp/Tugas/Tugas 12 - coreComponent/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function CoreComponent(){
+    return (
+        <View>
+            <Text>CoreComponent</Text>
+        </View>
+    )
+}
+
+
+
+const styles = StyleSheet.create({})
