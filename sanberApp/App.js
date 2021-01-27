c54aee53c3d2b479eb1f50f92a29a37c84d9dc60
RickyBR <berlandoricky@gmail.com>
2021-01-28 01:35:26 +0700

    update tugas

--- sanberApp/App.js
+++ sanberApp/App.js
@@ -7,14 +7,19 @@ import {
   TouchableOpacity,
   FlatList
 } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default function App() {
   return (
    <View style={styles.container}>
       <View style={styles.navBar}>
           <Image source={require('./assets/logo.png')} style={{width:100,height:22}}/>
-          <View style={styles.rightNav}>
+          <TouchableOpacity>
 
+          </TouchableOpacity>
+          <View style={styles.rightNav}>
+          <Icon style={styles.navItem} name="search" size={25}/>
+          <Icon style={styles.navItem} name="account-circle" size={25}/>
           </View>
       </View>
    </View>
@@ -32,6 +37,12 @@ const styles = StyleSheet.create({
     elevation: 3,
     paddingHorizontal : 15,
     flexDirection: 'row',
-    alignItems:'center'
+    alignItems:'center',
+    justifyContent:'space-between'
+  },rightNav:{
+    flexDirection:'row'
+  },
+  navItem: {
+    marginLeft:25
   }
 });
